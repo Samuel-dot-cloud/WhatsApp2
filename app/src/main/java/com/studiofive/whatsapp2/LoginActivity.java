ea52ecb6d4382f8e9e359e7bcc026c6c379f49f6
Samuel-dot-cloud <successam336@gmail.com>
2020-06-10 20:41:05 +0300

    creation of sign-up activity

--- app/src/main/java/com/studiofive/whatsapp2/LoginActivity.java
+++ app/src/main/java/com/studiofive/whatsapp2/LoginActivity.java
@@ -2,13 +2,30 @@ package com.studiofive.whatsapp2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseUser;
+
 public class LoginActivity extends AppCompatActivity {
+    private FirebaseUser mFirebaseUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (mFirebaseUser != null){
+            sendUserToMainActivity();
+        }
+    }
+
+    private void sendUserToMainActivity() {
+        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+        startActivity(intent);
     }
 }
