7f544014d175d7f39a4fc27b8e949775a50eda9f
Samuel-dot-cloud <successam336@gmail.com>
2020-06-11 10:31:54 +0300

    successfully signing up a user

--- app/src/main/java/com/studiofive/whatsapp2/LoginActivity.java
+++ app/src/main/java/com/studiofive/whatsapp2/LoginActivity.java
@@ -4,17 +4,50 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseUser;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class LoginActivity extends AppCompatActivity {
     private FirebaseUser mFirebaseUser;
+    @BindView(R.id.login_button)
+    Button mLoginButton;
+    @BindView(R.id.login_email)
+    EditText mLoginEmail;
+    @BindView(R.id.need_new_account)
+    TextView mNeedAccount;
+    @BindView(R.id.forget_password_link)
+    TextView mForgetPassword;
+    @BindView(R.id.login_password)
+    EditText mLoginPassword;
+    @BindView(R.id.login_phone_button)
+    Button mPhoneButton;
+    @BindView(R.id.login_image)
+    ImageView mLoginImage;
+    @BindView(R.id.login_using)
+    TextView mLoginUsing;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+
+        mNeedAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendUserToSignUpActivity();
+            }
+        });
     }
+
 
     @Override
     protected void onStart() {
@@ -26,6 +59,11 @@ public class LoginActivity extends AppCompatActivity {
 
     private void sendUserToMainActivity() {
         Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+        startActivity(intent);
+    }
+
+    private void sendUserToSignUpActivity() {
+        Intent intent = new Intent(LoginActivity.this, SignUpActivity.class);
         startActivity(intent);
     }
 }
--- app/src/main/java/com/studiofive/whatsapp2/SignUpActivity.java
+++ app/src/main/java/com/studiofive/whatsapp2/SignUpActivity.java
@@ -1,14 +1,99 @@
 package com.studiofive.whatsapp2;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class SignUpActivity extends AppCompatActivity {
+    @BindView(R.id.signUp_button)
+    Button mSignUpButton;
+    @BindView(R.id.signUp_email)
+    EditText mSignUpEmail;
+    @BindView(R.id.signUp_password)
+    EditText mSignUpPassword;
+    @BindView(R.id.already_have_account_link)
+    TextView mAlreadyAccount;
+    @BindView(R.id.signUp_image)
+    ImageView mSignUpImage;
+
+    private FirebaseAuth mAuth;
+    private ProgressDialog mProgressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        ButterKnife.bind(this);
+
+        mAuth = FirebaseAuth.getInstance();
+        mProgressDialog = new ProgressDialog(this);
+
+        mAlreadyAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendUserToLoginActivity();
+            }
+        });
+
+        mSignUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                createNewAccount();
+            }
+        });
+    }
+
+    private void createNewAccount() {
+        String email = mSignUpEmail.getText().toString();
+        String password = mSignUpPassword.getText().toString();
+
+        if (TextUtils.isEmpty(email) || TextUtils.isEmpty(password)){
+            Toast.makeText(this, "Please enter valid details!!", Toast.LENGTH_SHORT).show();
+        }
+        else {
+            mProgressDialog.setTitle("Creating New Account");
+            mProgressDialog.setMessage("Please wait while we are creating a new account for you...");
+            mProgressDialog.setCanceledOnTouchOutside(true);
+            mProgressDialog.show();
+            mAuth.createUserWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()){
+                                sendUserToLoginActivity();
+                                Toast.makeText(SignUpActivity.this, "Account created successfully.", Toast.LENGTH_SHORT).show();
+                            }
+                            else {
+                                String message = task.getException().toString();
+                                Toast.makeText(SignUpActivity.this, "Error: " + message, Toast.LENGTH_SHORT).show();
+
+                            }
+                            mProgressDialog.dismiss();
+                        }
+                    });
+        }
+    }
+
+    private void sendUserToLoginActivity() {
+        Intent intent = new Intent(SignUpActivity.this, LoginActivity.class);
+        startActivity(intent);
     }
 }
