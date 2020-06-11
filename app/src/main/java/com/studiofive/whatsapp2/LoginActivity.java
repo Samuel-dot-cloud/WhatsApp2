b5da6a0c125a8f600cd1453bd13fffa582499321
Samuel-dot-cloud <successam336@gmail.com>
2020-06-11 11:04:32 +0300

    successfully logging out a user

--- app/src/main/java/com/studiofive/whatsapp2/LoginActivity.java
+++ app/src/main/java/com/studiofive/whatsapp2/LoginActivity.java
@@ -1,15 +1,23 @@
 package com.studiofive.whatsapp2;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 import butterknife.BindView;
@@ -34,11 +42,19 @@ public class LoginActivity extends AppCompatActivity {
     @BindView(R.id.login_using)
     TextView mLoginUsing;
 
+    private FirebaseAuth mAuth;
+    private ProgressDialog mProgressDialog;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         ButterKnife.bind(this);
+
+        mAuth =FirebaseAuth.getInstance();
+        mFirebaseUser =mAuth.getCurrentUser();
+
+        mProgressDialog = new ProgressDialog(this);
 
         mNeedAccount.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -46,6 +62,42 @@ public class LoginActivity extends AppCompatActivity {
                 sendUserToSignUpActivity();
             }
         });
+
+        mLoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Login();
+            }
+        });
+    }
+
+    private void Login() {
+        String email = mLoginEmail.getText().toString();
+        String password = mLoginPassword.getText().toString();
+
+        if (TextUtils.isEmpty(email) || TextUtils.isEmpty(password)) {
+            Toast.makeText(this, "Please enter valid details!!", Toast.LENGTH_SHORT).show();
+        } else {
+            mProgressDialog.setTitle("Signing In");
+            mProgressDialog.setMessage("Please wait...");
+            mProgressDialog.setCanceledOnTouchOutside(true);
+            mProgressDialog.show();
+
+            mAuth.signInWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()){
+                                sendUserToMainActivity();
+                                Toast.makeText(LoginActivity.this, "Logged in Successfully!!", Toast.LENGTH_SHORT).show();
+                            }else {
+                                String message = task.getException().toString();
+                                Toast.makeText(LoginActivity.this, "Error: " + message, Toast.LENGTH_SHORT).show();
+                            }
+                            mProgressDialog.dismiss();
+                        }
+                    });
+        }
     }
 
 
--- app/src/main/java/com/studiofive/whatsapp2/MainActivity.java
+++ app/src/main/java/com/studiofive/whatsapp2/MainActivity.java
@@ -1,5 +1,6 @@
 package com.studiofive.whatsapp2;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.FragmentPagerAdapter;
@@ -8,9 +9,12 @@ import androidx.viewpager.widget.ViewPager;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 
 import com.google.android.material.tabs.TabLayout;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 import butterknife.BindView;
@@ -25,6 +29,7 @@ public class MainActivity extends AppCompatActivity {
     TabLayout mTabLayout;
    private TabsAccessorAdapter mTabsAccessorAdapter;
    private FirebaseUser mFirebaseUser;
+   private FirebaseAuth mAuth;
 
 
 
@@ -36,6 +41,9 @@ public class MainActivity extends AppCompatActivity {
 
         setSupportActionBar(mToolbar);
         getSupportActionBar().setTitle(Constants.APP_TITLE);
+
+        mAuth = FirebaseAuth.getInstance();
+        mFirebaseUser = mAuth.getCurrentUser();
 
         mTabsAccessorAdapter = new TabsAccessorAdapter(getSupportFragmentManager(), FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
         mViewPager.setAdapter(mTabsAccessorAdapter);
@@ -53,5 +61,30 @@ public class MainActivity extends AppCompatActivity {
     private void sendUserToLoginActivity() {
         Intent intent = new Intent(MainActivity.this, LoginActivity.class);
         startActivity(intent);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+         super.onCreateOptionsMenu(menu);
+
+        getMenuInflater().inflate(R.menu.options_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+         super.onOptionsItemSelected(item);
+
+         if (item.getItemId() == R.id.main_logout){
+             mAuth.signOut();
+             sendUserToLoginActivity();
+         }
+         if (item.getItemId() == R.id.main_settings){
+
+         }
+         if (item.getItemId() == R.id.main_find_friends){
+
+         }
+         return true;
     }
 }
