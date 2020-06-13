6ed068284cd30b31f10a816a3675d2f3d0f0f2d4
Samuel-dot-cloud <successam336@gmail.com>
2020-06-13 18:14:36 +0300

    addition of feature that makes send button invisible to current user

--- app/src/main/java/com/studiofive/whatsapp2/ProfileActivity.java
+++ app/src/main/java/com/studiofive/whatsapp2/ProfileActivity.java
@@ -1,12 +1,14 @@
 package com.studiofive.whatsapp2;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -19,7 +21,8 @@ import butterknife.ButterKnife;
 import de.hdodenhof.circleimageview.CircleImageView;
 
 public class ProfileActivity extends AppCompatActivity {
-    private String receiverUserId;
+    private String receiverUserId, current_state, currentUser;
+
     @BindView(R.id.visit_profile_image)
     CircleImageView mProfileImage;
     @BindView(R.id.visit_profile_name)
@@ -30,6 +33,7 @@ public class ProfileActivity extends AppCompatActivity {
     Button mSendMessageButton;
 
     private DatabaseReference mRef;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,12 +41,15 @@ public class ProfileActivity extends AppCompatActivity {
         setContentView(R.layout.activity_profile);
         ButterKnife.bind(this);
 
+        mAuth = FirebaseAuth.getInstance();
         mRef = FirebaseDatabase.getInstance().getReference().child("Users");
 
         receiverUserId = getIntent().getExtras().get("visit_user_id").toString();
-//        Toast.makeText(ProfileActivity.this, "User id: " + receiverUserId, Toast.LENGTH_SHORT).show();
+        currentUser = mAuth.getCurrentUser().getUid();
         
         retrieveUserInfo();
+
+        current_state = "new";
 
     }
 
@@ -59,6 +66,8 @@ public class ProfileActivity extends AppCompatActivity {
                     mProfileName.setText(userName);
                     mProfileStatus.setText(userStatus);
 
+                    manageChatRequests();
+
                 }
                 else {
                     String userName = dataSnapshot.child("name").getValue().toString();
@@ -67,6 +76,7 @@ public class ProfileActivity extends AppCompatActivity {
                     mProfileName.setText(userName);
                     mProfileStatus.setText(userStatus);
 
+                    manageChatRequests();
                 }
             }
 
@@ -75,5 +85,14 @@ public class ProfileActivity extends AppCompatActivity {
 
             }
         });
+    }
+
+    private void manageChatRequests() {
+        if(!currentUser.equals(receiverUserId)){
+
+        }
+        else {
+            mSendMessageButton.setVisibility(View.INVISIBLE);
+        }
     }
 }
