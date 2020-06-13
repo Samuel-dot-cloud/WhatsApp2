04e675f85e192f531c610c984c5c970199159800
Samuel-dot-cloud <successam336@gmail.com>
2020-06-13 16:45:58 +0300

    displaying all users of application

--- app/src/main/java/com/studiofive/whatsapp2/FindFriendsActivity.java
+++ app/src/main/java/com/studiofive/whatsapp2/FindFriendsActivity.java
@@ -1,14 +1,27 @@
 package com.studiofive.whatsapp2;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.squareup.picasso.Picasso;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class FindFriendsActivity extends AppCompatActivity {
     @BindView(R.id.find_friends_toolbar)
@@ -16,11 +29,15 @@ public class FindFriendsActivity extends AppCompatActivity {
     @BindView(R.id.find_friends_recycler_list)
     RecyclerView mFindFriendsList;
 
+    private DatabaseReference mRef;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find_friends);
         ButterKnife.bind(this);
+
+        mRef = FirebaseDatabase.getInstance().getReference().child("Users");
 
         mFindFriendsList.setLayoutManager(new LinearLayoutManager(this));
         setSupportActionBar(mToolbar);
@@ -28,5 +45,49 @@ public class FindFriendsActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         getSupportActionBar().setTitle("Find Friends");
 
+    }
+
+    @Override
+    protected void onStart() {
+
+        super.onStart();
+
+        FirebaseRecyclerOptions<Contacts> options = new FirebaseRecyclerOptions.Builder<Contacts>()
+                .setQuery(mRef, Contacts.class)
+                .build();
+
+        FirebaseRecyclerAdapter<Contacts, FindFriendsViewHolder> adapter = new FirebaseRecyclerAdapter<Contacts, FindFriendsViewHolder>(options) {
+            @Override
+            protected void onBindViewHolder(@NonNull FindFriendsViewHolder holder, int position, @NonNull Contacts model) {
+                holder.mProfileName.setText(model.getName());
+                holder.mUserStatus.setText(model.getStatus());
+                Picasso.get().load(model.getImage()).into(holder.mUserProfileImage);
+            }
+
+            @NonNull
+            @Override
+            public FindFriendsViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+              View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.users_display_layout, parent, false);
+              FindFriendsViewHolder viewHolder = new FindFriendsViewHolder(view);
+              return viewHolder;
+            };
+        };
+
+        mFindFriendsList.setAdapter(adapter);
+        adapter.startListening();
+    }
+
+    public static class FindFriendsViewHolder extends RecyclerView.ViewHolder{
+        @BindView(R.id.user_profile_name)
+        TextView mProfileName;
+        @BindView(R.id.user_status)
+        TextView mUserStatus;
+        @BindView(R.id.users_profile_image)
+        CircleImageView mUserProfileImage;
+
+        public FindFriendsViewHolder(@NonNull View itemView) {
+            super(itemView);
+            ButterKnife.bind(this, itemView);
+        }
     }
 }
